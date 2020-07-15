b3a59e3467ffd23f7664d6ada2d47c8bcdf9feb3
kearneys12 <63612706+kearneys12@users.noreply.github.com>
2020-07-14 23:28:08 -0400

    Add files via upload

---
+++ capstone/TenmoClient/AuthService.cs
@@ -0,0 +1,257 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using TenmoClient.Data;
+
+namespace TenmoClient
+{
+    public class AuthService
+    {
+        private readonly static string API_BASE_URL = "https://localhost:44315/";
+        private readonly IRestClient client = new RestClient();
+        bool addFundsResult = false;
+
+        //login endpoints
+        public bool Register(LoginUser registerUser)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "login/register");
+            request.AddJsonBody(registerUser);
+            IRestResponse<API_User> response = client.Post<API_User>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                if (!string.IsNullOrWhiteSpace(response.Data.Message))
+                {
+                    throw new Exception("An error message was received: " + response.Data.Message);
+                }
+                else
+                {
+                    throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public API_User Login(LoginUser loginUser)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "login");
+            request.AddJsonBody(loginUser);
+            IRestResponse<API_User> response = client.Post<API_User>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+                if (!string.IsNullOrWhiteSpace(response.Data.Message))
+                {
+                    throw new Exception("An error message was received: " + response.Data.Message);
+                }
+                else
+                {
+                    throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                }
+
+            }
+            else
+            {
+                client.Authenticator = new JwtAuthenticator(response.Data.Token);
+                return response.Data;
+            }
+        }
+        public int GetBalance(int userId)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transaction/"+ userId);
+            IRestResponse<int> response = client.Get<int>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+               
+                    throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+            
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public string GetUsers()
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transaction/users");
+            IRestResponse<List<string>> response = client.Get<List<string>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+
+                throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+            }
+            else
+            {
+                string userList = "";
+                foreach (string user in response.Data)
+                {
+                    userList += user + Environment.NewLine;
+                    
+                }
+                return userList;
+            }
+
+        }
+
+        public bool AddFunds(decimal transferAmount, int transferRecipientID)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + $"transaction/add/{transferRecipientID}/{transferAmount}");
+            IRestResponse<bool> response = client.Put<bool>(request);
+
+            try
+            {
+                if (response.ResponseStatus != ResponseStatus.Completed)
+                {
+                    throw new Exception("An error occurred communicating with the server.");
+
+                }
+                else if (!response.IsSuccessful)
+                {
+
+                    throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+                }
+                else
+                {
+                    addFundsResult = true;
+                    return response.Data;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+        public bool TransferFunds(decimal transferAmount, int userId, int transferRecipientId)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + $"transaction/deduct/{userId}/{transferAmount}/{transferRecipientId}");
+            IRestResponse<bool> response = client.Put<bool>(request);
+
+            if (GetBalance(userId) < transferAmount || addFundsResult == false)
+            {
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    if (response.ResponseStatus != ResponseStatus.Completed)
+                    {
+                        throw new Exception("An error occurred communicating with the server.");
+
+                    }
+                    else if (!response.IsSuccessful)
+                    {
+
+                        throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+                    }
+                    else
+                    {
+
+                        return response.Data;
+                    }
+
+                }
+                catch
+                {
+                    return false;
+                }
+            }           
+        }
+        public List<string> GetAllTransfers(int account)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transaction/transfers/" + account);
+            IRestResponse<List<string>> response = client.Get<List<string>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+
+                throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public List<string> GetSingleTransfer(string transferId)
+        {
+            RestRequest request = new RestRequest(API_BASE_URL + "transaction/transfers/specific/" + transferId);
+            IRestResponse<List<string>> response = client.Get<List<string>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+
+                throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public int GetTransferId()
+        {
+            RestRequest request = new RestRequest (API_BASE_URL + "transaction/transferId");
+            IRestResponse<int> response = client.Get<int>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("An error occurred communicating with the server.");
+
+            }
+            else if (!response.IsSuccessful)
+            {
+
+                throw new Exception("An error response was received from the server. The status code is " + (int)response.StatusCode);
+
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+        //write server call to collect transfer id (Steve)
+    }
+}
---
+++ capstone/TenmoClient/ConsoleService.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using TenmoClient.Data;
+
+
+namespace TenmoClient
+{
+    public class ConsoleService
+    {
+        private static readonly AuthService authService = new AuthService();
+        string username;
+
+        public void Run()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to TEnmo!");
+                Console.WriteLine("1: Login");
+                Console.WriteLine("2: Register");
+                Console.WriteLine("3: Exit");
+                Console.Write("Please choose an option: ");
+
+                int loginRegister = -1;
+
+                try
+                {
+                    if (!int.TryParse(Console.ReadLine(), out loginRegister))
+                    {
+                        Console.WriteLine("Invalid input. Please enter only a number.");
+                    }
+
+                    else if (loginRegister == 1)
+                    {
+                        LoginUser loginUser = PromptForLogin();
+                        API_User user = authService.Login(loginUser);
+                        if (user != null)
+                        {
+                            UserService.SetLogin(user);
+                            MenuSelection();
+                        }
+                    }
+
+                    else if (loginRegister == 2)
+                    {
+                        LoginUser registerUser = PromptForLogin();
+                        bool isRegistered = authService.Register(registerUser);
+                        if (isRegistered)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Registration successful. You can now log in.");
+                        }
+                    }
+
+                    else if (loginRegister == 3)
+                    {
+                        Console.WriteLine("Goodbye!");
+                        Environment.Exit(0);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid selection.");
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error - " + ex.Message);
+                }
+            }
+        }
+
+        private void MenuSelection()
+        {
+            int menuSelection = -1;
+            while (menuSelection != 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Welcome to TEnmo! Please make a selection: ");
+                Console.WriteLine("1: View your current balance");
+                Console.WriteLine("2: View your past transfers"); //view details through here
+                Console.WriteLine("3: View your pending requests"); //ability to approve/reject through here
+                Console.WriteLine("4: Send TE bucks");
+                Console.WriteLine("5: Request TE bucks");
+                Console.WriteLine("6: View list of users");
+                Console.WriteLine("7: Log in as different user");
+                Console.WriteLine("0: Exit");
+                Console.WriteLine("---------");
+                Console.Write("Please choose an option: ");
+
+                try
+                {
+                    if (!int.TryParse(Console.ReadLine(), out menuSelection))
+                    {
+                        Console.WriteLine("Invalid input. Please enter only a number.");
+                    }
+                    else if (menuSelection == 1)
+                    {
+                        int userId = UserService.GetUserId();
+                        int accountBalance = authService.GetBalance(userId);
+                        Console.WriteLine("Account balance = " + accountBalance);
+                    }
+                    else if (menuSelection == 2)
+                    {
+                        Console.WriteLine("Enter transfer Id or enter 0 to see all of your past transactions");
+                        string userInput = Console.ReadLine();
+                        if (userInput == "0")
+                        {
+                            int userId = UserService.GetUserId();
+                            List<string> transferDetails = authService.GetAllTransfers(userId);
+                            foreach (string item in transferDetails)
+                            {
+                                Console.WriteLine("Transfer Details: " + item);
+                            }
+                        }
+                        else
+                        {
+                            
+                            List<string> transferDetails = authService.GetSingleTransfer(userInput);
+                            foreach (string item in transferDetails)
+                            {
+                                Console.WriteLine("Transfer Details: " + item);
+                            }
+                        }
+
+                    }
+                    else if (menuSelection == 3)
+                    {
+
+                    }
+                    else if (menuSelection == 4)
+                    {
+                        decimal transferAmount = 0;
+                        Console.WriteLine("Please enter the amount of TE Bucks to send:");
+                        string stringTransferAmount = Console.ReadLine();
+
+                        try
+                        {
+                            transferAmount = Convert.ToDecimal(stringTransferAmount);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine(Environment.NewLine + "ERROR: Please enter an integer amount to be transferred");
+                        }
+
+                        Console.WriteLine("Please enter the User ID of the recipient");
+
+                        string stringtransferRecipientId = Console.ReadLine();
+                        int transferRecipientId = 0;
+                        try
+                        {
+                            transferRecipientId = Convert.ToInt32(stringtransferRecipientId);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine(Environment.NewLine + "ERROR: Please enter an ID using only integers");
+                        }
+
+                        int userId = UserService.GetUserId();
+
+                        bool addFundsSuccess = authService.AddFunds(transferAmount, transferRecipientId);
+                        bool transferSuccess = authService.TransferFunds(transferAmount, userId, transferRecipientId);
+
+                        if (transferSuccess == true && addFundsSuccess == true)
+                        {
+                            int transferId = authService.GetTransferId();
+                            Console.WriteLine("transfer successful");
+                            Console.WriteLine("Please keep for your records. Transfer Id = " + transferId);
+                        }
+                        else
+                        {
+                            Console.WriteLine("transfer unable to be completed successfully");
+                        }
+                    }
+                    else if (menuSelection == 5)
+                    {
+
+                    }
+                    else if (menuSelection == 6)
+                    {
+                        string users = authService.GetUsers();
+                        Console.WriteLine(users);
+
+                    }
+                    else if (menuSelection == 7)
+                    {
+                        Console.WriteLine("");
+                        UserService.SetLogin(new API_User()); //wipe out previous login info
+                        return; //return to register/login menu
+                    }
+                    else if (menuSelection == 0)
+                    {
+                        Console.WriteLine("Goodbye!");
+                        Environment.Exit(0);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Please try again");
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error - " + ex.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public int PromptForTransferID(string action)
+        {
+            Console.WriteLine("");
+            Console.Write("Please enter transfer ID to " + action + " (0 to cancel): ");
+            if (!int.TryParse(Console.ReadLine(), out int auctionId))
+            {
+                Console.WriteLine("Invalid input. Only input a number.");
+                return 0;
+            }
+            else
+            {
+                return auctionId;
+            }
+        }
+
+        public LoginUser PromptForLogin()
+        {
+            Console.Write("Username: ");
+            username = Console.ReadLine();
+            string password = GetPasswordFromConsole("Password: ");
+
+            LoginUser loginUser = new LoginUser
+            {
+                Username = username,
+                Password = password
+            };
+            return loginUser;
+        }
+
+        private string GetPasswordFromConsole(string displayMessage)
+        {
+            string pass = "";
+            Console.Write(displayMessage);
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Backspace Should Not Work
+                if (!char.IsControl(key.KeyChar))
+                {
+                    pass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        pass = pass.Remove(pass.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            // Stops Receving Keys Once Enter is Pressed
+            while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine("");
+            return pass;
+        }
+    }
+}
